7ff048c844a2921abc68408286443291698c6b64
russomaclaud <66261562+russomaclaud@users.noreply.github.com>
2020-07-07 06:54:06 +0200

    Agregamos el GenericRepository y creamos la EntityModel Country

---
+++ Shop.Web/Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Web.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+
+    public class User : IdentityUser
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -4,15 +4,20 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Helpers;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore.Internal;
 
     public class SeedDb
     {
         private readonly DataContext context;
-        private Random random;
+        private readonly IUserHelper userHelper;
+        private readonly Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             this.context = context;
+            this.userHelper = userHelper;
             this.random = new Random();
         }
 
@@ -20,23 +25,45 @@
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userHelper.GetUserByEmailAsync("erussomaclaud@gmail.com");
+
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Efren",
+                    LastName = "Quintero Salazar",
+                    Email = "erussomaclaud@gmail.com",
+                    UserName = "erussomaclaud@gmail.com",
+                    PhoneNumber = "3006039023"
+                };
+
+                var result = await this.userHelper.AddUserAsync(user, "123456");
+
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("iPhone X");
-                this.AddProduct("Magic Mouse");
-                this.AddProduct("iWatch Series 4");
+                this.AddProduct("iPhone X", user);
+                this.AddProduct("Magic Mouse", user);
+                this.AddProduct("iWatch Series 4", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(1000),
                 IsAvailabe = true,
-                Stock = this.random.Next(100)
+                Stock = this.random.Next(100),
+                User = user
             });
         }
     }
